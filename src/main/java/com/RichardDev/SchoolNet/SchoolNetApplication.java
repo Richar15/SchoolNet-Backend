51e7dc31842eb45a789c8853655693a9120aa38e
Richar15 <rassist@unicartagena.edu.co>
2025-06-09 10:01:30 -0500

    first commit

---
+++ src/main/java/com/RichardDev/SchoolNet/SchoolNetApplication.java
@@ -0,0 +1,13 @@
+package com.RichardDev.SchoolNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RichardDev/SchoolNet/SchoolNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RichardDev.SchoolNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
